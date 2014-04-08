34fb2df86e25e9fdf6acd970b6dd4213d38a950a
Steve Kondik <shade@chemlab.org>
2014-04-07 17:41:49 -0700

    Revert swaprect feature for new upstream implementation.
    Revert "Display: Define new composition type for blit composition"
    
    This reverts commit 835f0ad6da60ad5cd5f8d6d872c5665aae6f5584.
    
    Revert "surfaceflinger: Conditional compilation for swaprect feature"
    
    This reverts commit 5e9e33ac72e608d584b13bea176f8fc7f30d88e7.
    
    Conflicts:
    	services/surfaceflinger/DisplayHardware/HWComposer.h
    
    Change-Id: I83e3b5a66b9ea7c3bae008d6a7e84c1358c512bc
    
    Revert "SF: Add flag for swaprect status in HAL"
    
    This reverts commit 4fd75c0a371d4ee04e0f583d790b576494257ea8.
    
    Conflicts:
    	services/surfaceflinger/DisplayHardware/HWComposer.cpp
    
    Change-Id: Ibd8714a49e09653bf34da466db17fbe4b20c6c8d
    
    Revert "sf: Enable swaprect optimization by default only on QCOMs"
    
    This reverts commit 0438f810c7e8e3c53850874c7bdb74ac43b2ec88.
    
    Revert "Display: Add swaprect feature for MDP composition."
    
    This reverts commit 9b5ef3793b6012e979c10e4cc9a7e091092a4a57.
    
    Conflicts:
    	include/gui/Surface.h
    
    Change-Id: I6d276ee82f8f0b4214e79c91cd9675fcbfd57dd4

--- libs/gui/GLConsumer.cpp
+++ libs/gui/GLConsumer.cpp
@@ -532,8 +532,6 @@ status_t GLConsumer::updateAndReleaseLocked(const BufferQueue::BufferItem& item)
 
     computeCurrentTransformMatrixLocked();
 
-    mConsumer->setCurrentDirtyRegion(buf);
-
     return err;
 }
 
@@ -1138,11 +1136,6 @@ status_t GLConsumer::doGLFenceWaitLocked() const {
     }
 
     return NO_ERROR;
-}
-
-void GLConsumer::getDirtyRegion(Rect& dirtyRect) {
-     Mutex::Autolock lock(mMutex);
-     mConsumer->getCurrentDirtyRegion(dirtyRect);
 }
 
 void GLConsumer::freeBufferLocked(int slotIndex) {
