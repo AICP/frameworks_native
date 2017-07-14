86b412fa74ed704a711b445ed325157f39206c69
Moshe Barash <mosimchah@gmail.com>
2017-07-14 20:24:08 +0000

    Revert "Remove ATRACE from SurfaceFlinger"
    Apparently some devices need this
    This reverts commit 1062638d0860bf807786482ae81077a15fd3aefc.
    
    Change-Id: I2be06b68caf600c820e2e767bba1fe9dac2cfa27

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -54,6 +54,7 @@
 #include <utils/String16.h>
 #include <utils/StopWatch.h>
 #include <utils/Timers.h>
+#include <utils/Trace.h>
 
 #include <private/android_filesystem_config.h>
 #include <private/gui/SyncFeatures.h>
@@ -377,6 +378,7 @@ public:
                 ALOGE("error registering vsync callback: %s (%d)",
                         strerror(-err), err);
             }
+            //ATRACE_INT(mVsyncOnLabel.string(), 1);
         } else {
             status_t err = mDispSync->removeEventListener(
                     static_cast<DispSync::Callback*>(this));
@@ -384,6 +386,7 @@ public:
                 ALOGE("error unregistering vsync callback: %s (%d)",
                         strerror(-err), err);
             }
+            //ATRACE_INT(mVsyncOnLabel.string(), 0);
         }
         mEnabled = enable;
     }
@@ -434,6 +437,11 @@ private:
         {
             Mutex::Autolock lock(mCallbackMutex);
             callback = mCallback;
+
+            if (mTraceVsync) {
+                mValue = (mValue + 1) % 2;
+                ATRACE_INT(mVsyncEventLabel.string(), mValue);
+            }
         }
 
         if (callback != NULL) {
@@ -1035,16 +1043,19 @@ void SurfaceFlinger::onHotplugReceived(int32_t disp, bool connected) {
 }
 
 void SurfaceFlinger::setVsyncEnabled(int disp, int enabled) {
+    ATRACE_CALL();
     getHwComposer().setVsyncEnabled(disp,
             enabled ? HWC2::Vsync::Enable : HWC2::Vsync::Disable);
 }
 
 void SurfaceFlinger::onMessageReceived(int32_t what) {
+    ATRACE_CALL();
     switch (what) {
         case MessageQueue::INVALIDATE: {
             bool frameMissed = !mHadClientComposition &&
                     mPreviousPresentFence != Fence::NO_FENCE &&
                     mPreviousPresentFence->getSignalTime() == INT64_MAX;
+            ATRACE_INT("FrameMissed", static_cast<int>(frameMissed));
             if (mPropagateBackpressure && frameMissed) {
                 signalLayerUpdate();
                 break;
@@ -1078,10 +1089,12 @@ bool SurfaceFlinger::handleMessageTransaction() {
 }
 
 bool SurfaceFlinger::handleMessageInvalidate() {
+    ATRACE_CALL();
     return handlePageFlip();
 }
 
 void SurfaceFlinger::handleMessageRefresh() {
+    ATRACE_CALL();
 
     nsecs_t refreshStartTime = systemTime(SYSTEM_TIME_MONOTONIC);
 
@@ -1157,6 +1170,7 @@ void SurfaceFlinger::doDebugFlashRegions()
 
 void SurfaceFlinger::preComposition()
 {
+    ATRACE_CALL();
     ALOGV("preComposition");
 
     bool needExtraInvalidate = false;
@@ -1174,6 +1188,7 @@ void SurfaceFlinger::preComposition()
 
 void SurfaceFlinger::postComposition(nsecs_t refreshStartTime)
 {
+    ATRACE_CALL();
     ALOGV("postComposition");
 
     const LayerVector& layers(mDrawingState.layersSortedByZ);
@@ -1243,10 +1258,12 @@ void SurfaceFlinger::postComposition(nsecs_t refreshStartTime)
 }
 
 void SurfaceFlinger::rebuildLayerStacks() {
+    ATRACE_CALL();
     ALOGV("rebuildLayerStacks");
 
     // rebuild the visible layer list per screen
     if (CC_UNLIKELY(mVisibleRegionsDirty)) {
+        ATRACE_CALL();
         mVisibleRegionsDirty = false;
         invalidateHwcGeometry();
 
@@ -1292,6 +1309,7 @@ void SurfaceFlinger::rebuildLayerStacks() {
 }
 
 void SurfaceFlinger::setUpHWComposer() {
+    ATRACE_CALL();
     ALOGV("setUpHWComposer");
 
     for (size_t dpy=0 ; dpy<mDisplays.size() ; dpy++) {
@@ -1389,6 +1407,7 @@ void SurfaceFlinger::setUpHWComposer() {
 }
 
 void SurfaceFlinger::doComposition() {
+    ATRACE_CALL();
     ALOGV("doComposition");
 
     const bool repaintEverything = android_atomic_and(0, &mRepaintEverything);
@@ -1413,6 +1432,7 @@ void SurfaceFlinger::doComposition() {
 
 void SurfaceFlinger::postFramebuffer()
 {
+    ATRACE_CALL();
     ALOGV("postFramebuffer");
 
     const nsecs_t now = systemTime();
@@ -1462,6 +1482,7 @@ void SurfaceFlinger::postFramebuffer()
 
 void SurfaceFlinger::handleTransaction(uint32_t transactionFlags)
 {
+    ATRACE_CALL();
 
     // here we keep a copy of the drawing state (that is the state that's
     // going to be overwritten by handleTransactionLocked()) outside of
@@ -1819,6 +1840,7 @@ void SurfaceFlinger::computeVisibleRegions(size_t /*dpy*/,
         const LayerVector& currentLayers, uint32_t layerStack,
         Region& outDirtyRegion, Region& outOpaqueRegion)
 {
+    ATRACE_CALL();
     ALOGV("computeVisibleRegions");
 
     Region aboveOpaqueLayers;
@@ -2275,6 +2297,7 @@ void SurfaceFlinger::setTransactionState(
         const Vector<DisplayState>& displays,
         uint32_t flags)
 {
+    ATRACE_CALL();
     Mutex::Autolock _l(mStateLock);
     uint32_t transactionFlags = 0;
 
@@ -3621,6 +3644,7 @@ void SurfaceFlinger::renderScreenImplLocked(
         uint32_t minLayerZ, uint32_t maxLayerZ,
         bool yswap, bool useIdentityTransform, Transform::orientation_flags rotation)
 {
+    ATRACE_CALL();
     RenderEngine& engine(getRenderEngine());
 
     // get screen geometry
@@ -3689,6 +3713,7 @@ status_t SurfaceFlinger::captureScreenImplLocked(
         bool useIdentityTransform, Transform::orientation_flags rotation,
         bool isLocalScreenshot, bool useReadPixels)
 {
+    ATRACE_CALL();
 
     // get screen geometry
     uint32_t hw_w = hw->getWidth();
