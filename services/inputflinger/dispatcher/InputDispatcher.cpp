252cbf1a9b4e966e2b0eaaf3096d018ad92af88e
phpBB3 <alan.dowden@gmail.com>
2021-07-29 09:14:51 -0230

    Revert "Use the original device ID when the injected event is unchanged"
    This reverts commit 4104829ae98fb3abc59ae452bd56d5a12fd39a0a.
    
    Change-Id: I83025aeb8085e8219318e3210f0ce146a47c28d0

--- services/inputflinger/dispatcher/InputDispatcher.cpp
+++ services/inputflinger/dispatcher/InputDispatcher.cpp
@@ -3286,8 +3286,6 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
     if (hasInjectionPermission(injectorPid, injectorUid)) {
         policyFlags |= POLICY_FLAG_TRUSTED;
     }
-    // Override device ID if the injected event is not an unmodified real input.
-    bool forceVirtualKeyboardId = !(policyFlags & POLICY_FLAGS_INJECTED_IS_UNCHANGED);
 
     std::queue<EventEntry*> injectedEntries;
     switch (event->getType()) {
@@ -3300,14 +3298,11 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
 
             int32_t flags = incomingKey.getFlags();
             int32_t keyCode = incomingKey.getKeyCode();
-            int32_t deviceId = forceVirtualKeyboardId
-                               ? VIRTUAL_KEYBOARD_ID
-                               : incomingKey.getDeviceId();
             int32_t metaState = incomingKey.getMetaState();
-            accelerateMetaShortcuts(deviceId, action,
+            accelerateMetaShortcuts(VIRTUAL_KEYBOARD_ID, action,
                                     /*byref*/ keyCode, /*byref*/ metaState);
             KeyEvent keyEvent;
-            keyEvent.initialize(incomingKey.getId(), deviceId, incomingKey.getSource(),
+            keyEvent.initialize(incomingKey.getId(), VIRTUAL_KEYBOARD_ID, incomingKey.getSource(),
                                 incomingKey.getDisplayId(), INVALID_HMAC, action, flags, keyCode,
                                 incomingKey.getScanCode(), metaState, incomingKey.getRepeatCount(),
                                 incomingKey.getDownTime(), incomingKey.getEventTime());
@@ -3328,7 +3323,7 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
             mLock.lock();
             KeyEntry* injectedEntry =
                     new KeyEntry(incomingKey.getId(), incomingKey.getEventTime(),
-                                 deviceId, incomingKey.getSource(),
+                                 VIRTUAL_KEYBOARD_ID, incomingKey.getSource(),
                                  incomingKey.getDisplayId(), policyFlags, action, flags, keyCode,
                                  incomingKey.getScanCode(), metaState, incomingKey.getRepeatCount(),
                                  incomingKey.getDownTime());
@@ -3346,9 +3341,6 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
             if (!validateMotionEvent(action, actionButton, pointerCount, pointerProperties)) {
                 return INPUT_EVENT_INJECTION_FAILED;
             }
-            int32_t deviceId = forceVirtualKeyboardId
-                               ? VIRTUAL_KEYBOARD_ID
-                               : motionEvent->getDeviceId();
 
             if (!(policyFlags & POLICY_FLAG_FILTERED)) {
                 nsecs_t eventTime = motionEvent->getEventTime();
@@ -3364,7 +3356,7 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
             const nsecs_t* sampleEventTimes = motionEvent->getSampleEventTimes();
             const PointerCoords* samplePointerCoords = motionEvent->getSamplePointerCoords();
             MotionEntry* injectedEntry =
-                    new MotionEntry(motionEvent->getId(), *sampleEventTimes, deviceId,
+                    new MotionEntry(motionEvent->getId(), *sampleEventTimes, VIRTUAL_KEYBOARD_ID,
                                     motionEvent->getSource(), motionEvent->getDisplayId(),
                                     policyFlags, action, actionButton, motionEvent->getFlags(),
                                     motionEvent->getMetaState(), motionEvent->getButtonState(),
@@ -3381,7 +3373,7 @@ int32_t InputDispatcher::injectInputEvent(const InputEvent* event, int32_t injec
                 samplePointerCoords += pointerCount;
                 MotionEntry* nextInjectedEntry =
                         new MotionEntry(motionEvent->getId(), *sampleEventTimes,
-                                        deviceId, motionEvent->getSource(),
+                                        VIRTUAL_KEYBOARD_ID, motionEvent->getSource(),
                                         motionEvent->getDisplayId(), policyFlags, action,
                                         actionButton, motionEvent->getFlags(),
                                         motionEvent->getMetaState(), motionEvent->getButtonState(),
